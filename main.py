d7e3f80f33181d24b6d9480a566f1885e5a9dda2
zhetkynshekov <zhrk2407@gmail.com>
2020-12-02 19:04:55 +0500

    ADD links on page

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///InitFlask.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    price = db.Column(db.Integer, nullable = False)
+    isActive = db.Column(db.Boolean, default = True)
+
 
 @app.route('/')
 def index():
@@ -8,8 +19,13 @@ def index():
 
 
 @app.route('/about')
-def index():
-    return render_template('index.html')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
